0923b8ea6636f47a3d054f2447eedf4b683fcf3f
petja93 <peter.sokolov@haw-hamburg.de>
2017-05-19 15:12:20 +0200

    Coin und Skript hinzugefügt

---
+++ MobileGame/Assets/Scripts/StarRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarRotation : MonoBehaviour {
+
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed = 2;
+		transform.Rotate (0, speed, 0, Space.World);
+	}
+}
