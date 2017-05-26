130c8c55abcfde4353a0f1511a5bda73be8297cb
petja93 <peter.sokolov@haw-hamburg.de>
2017-05-26 14:27:52 +0200

    Deathscript und Todesanzeige hinzugefügt

---
+++ MobileGame/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour {
+
+	public GameObject DeathBlack;
+	public GameObject DeathText;
+	public GameObject TheRocket;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider col){
+		DeathBlack.SetActive (true);
+		DeathText.SetActive (true);
+		TheRocket.SetActive (false);
+	}
+}
