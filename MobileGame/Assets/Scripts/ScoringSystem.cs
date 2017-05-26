9e26e92b3232efba969bac90df508e59bf2f73b6
viderko <olena.malyshko@haw-hamburg.de>
2017-05-26 10:15:40 +0200

    Score hinzugefügt

---
+++ MobileGame/Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour {
+
+	static int theScore;
+	public GameObject scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("addScore", 1, 0.1F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void addScore(){
+		theScore += 0;
+		scoreDisplay.GetComponent<Text>().text = "Score: " + theScore;
+	}
+}
