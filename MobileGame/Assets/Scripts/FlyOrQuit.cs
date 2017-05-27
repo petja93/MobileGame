becccd9d6c6c6816c6bcb7d08a2f97fa3bdbed7d
petja93 <peter.sokolov@haw-hamburg.de>
2017-05-27 17:06:43 +0200

    Menü erstellt

---
+++ MobileGame/Assets/Scripts/FlyOrQuit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyOrQuit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnGUI(){
+		if (GUI.Button (new Rect (Screen.width / 2 , Screen.height/2 - 50, 100, 30), "Fly Again")) {
+			Application.LoadLevel (2);
+		}
+
+		if (GUI.Button (new Rect (Screen.width / 2, Screen.height/2 + 50, 100, 30), "Main Menu")) {
+			Application.LoadLevel (0);
+		}
+	}
+}
