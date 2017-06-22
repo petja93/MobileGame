cd74e7e605207fe9554aa7def9057a49186ccb91
petja93 <peter.sokolov@haw-hamburg.de>
2017-06-22 15:52:46 +0200

    Lebensanzeige hinzugefügt

--- MobileGame/Assets/Scripts/DeathScript.cs
+++ MobileGame/Assets/Scripts/DeathScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeathScript : MonoBehaviour {
 
@@ -11,13 +12,44 @@ public class DeathScript : MonoBehaviour {
 	public GameObject mainAudio;
 	public GameObject highScore;
 
+	public Sprite sprite2;
+	public Sprite sprite1;
+	public Sprite sprite0;
+
+	public Image image;
+
+
+	public static int health = 3; //die beiden Variablen müssen statisch sein, da sonst die Rechnnung nicht funktioniert
+	public static int amount = 1; //Ein weiterer Vorteil ist, wenn man die Variablen als static deklariert, kann man aus anderen Skripten darauf zugreifen
+
 	public void OnTriggerEnter(Collider col){
-		NextAxes.theXAxis = -1.0F;
-		highScore.SetActive (true);
-		crash.Play ();
-		mainAudio.SetActive (false);
-		DeathBlack.SetActive (true);
-		DeathText.SetActive (true);
-		TheRocket.SetActive (false);
+
+		if (health <= 1) {
+			health = health - amount;
+			changeImage (health);
+			Debug.Log (" anderes getriggered");
+			NextAxes.theXAxis = -1.0F;
+			highScore.SetActive (true);
+			crash.Play ();
+			mainAudio.SetActive (false);
+			DeathBlack.SetActive (true);
+			DeathText.SetActive (true);
+			TheRocket.SetActive (false);
+		} else {
+			Debug.Log ("getriggered");
+			health = health - amount;
+			changeImage (health);
+		}
+
+	}
+
+	public void changeImage(int health){
+		if (health == 2) {
+			image.sprite = sprite2;
+		} else if (health == 1) {
+			image.sprite = sprite1;
+		} else if (health == 0) {
+			image.sprite = sprite0;
+		}
 	}
 }
